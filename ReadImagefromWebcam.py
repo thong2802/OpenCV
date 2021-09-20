b1498205b086eb3a924cafb236a17e33ee538348
thong2802 <ducthong2802@gmail.com>
2021-09-21 00:56:38 +0700

    upfile

---
+++ ReadImagefromWebcam.py
@@ -0,0 +1,17 @@
+import cv2
+camera_id = 'sample.avi'
+# mo camera
+cap = cv2.VideoCapture(camera_id)
+#doc anh tu webcam
+while(True):
+    #doc anh
+    ret, frame = cap.read()
+    #hien anh
+    cv2.imshow("Anh", frame)
+
+    # kiem tra neu bam q thi thoat
+    if cv2.waitKey() & 0xFF == ord ('q'):
+        break
+#giai phong camera
+cap.release()
+cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import cv2
+
+# doc anh den trang
+image = cv2.imread("sample-2.png", cv2.IMREAD_GRAYSCALE)
+#show anh
+cv2.imshow("Anh", image)
+# dung man hinh
+cv2.waitKey()
+#dong tat ca cua so lai
+cv2.destroyAllWindows()
+
